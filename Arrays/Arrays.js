9df1af65dc4600be586b0a5566c052f9edabdb9c
Jesus Villalobos <jesusvillalobos2807@outlook.es>
2020-02-16 20:20:00 -0700

    Learning arrays

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -1,3 +1,27 @@
 'use strict'
 
-var name = ["Jesús", "Héctor","Alberto","Mario","Lea"];
+var Languages = ["Java", "Python", "C", "C++", "C#", "Ruby", "Go"];
+
+//var Element = parseInt(prompt("Ecoje un ID para seleccionar un lenguaje de programación...", 0));
+
+//DocumentWrite(Languages,Element);
+PrintArray(Languages);
+
+
+function DocumentWrite(Languages, Element) {
+    if (Element >= Languages.length) {
+        alert("Introduce un ID valido");
+    } else {
+        var Text = `<p>Selecciono el lenguaje: ${Languages[Element]}</p>`
+        document.write(Text);
+    }
+}
+
+function PrintArray(Languages) {
+    var T1 =`<p align="center">Lenguajes de programación.`
+    document.write(T1);
+    var T2 = ``
+    for (let index = 0; index < Languages.length; index++) {
+    }
+    document.write(T2);
+}
