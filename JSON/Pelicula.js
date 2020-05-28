0994cb68928bbdf516d00e6ec9756047f1a12544
Jesus Villalobos <jesusvillalobos2807@outlook.es>
2020-05-28 01:28:03 -0700

    Json

--- JSON/Pelicula.js
+++ JSON/Pelicula.js
@@ -1,10 +1,25 @@
 'use strict'
-
-    var Pelicula = {
+window.addEventListener("load", () => {
+    var Movie = {
         Titulo: 'Batman',
         Año: 2018,
         Pais: 'México'
     };
 
-    console.log(Pelicula);
+    var Films = [
+        {Titulo:"Wiplash", Año:2015,Pais:"U.S.A"},
+        Movie
+    ];
+
+    var MoviesBox = document.querySelector("#ID_MoviesBox");
+    var Index;
+    for (Index in Films) {
+        var P = document.createElement("p");
+        P.append(Films[Index].Titulo);
+        //MoviesBox.append(P);
+        console.log(P);
+        MoviesBox.append(P);
+    }
+
+});
     
